ad64b246fa2a2f8fc60ef639f2ee19cad02ed57b
Marc de Jonge <marc.dejonge@tno.nl>
2013-10-07 14:44:11 +0200

    Added a builder to the ConstraintList (with test)

--- flexiblepower.api/src/org/flexiblepower/rai/values/ConstraintList.java
+++ flexiblepower.api/src/org/flexiblepower/rai/values/ConstraintList.java
@@ -1,22 +1,59 @@
 package org.flexiblepower.rai.values;
 
 import java.util.AbstractList;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.measure.Measurable;
+import javax.measure.Measure;
 import javax.measure.quantity.Quantity;
+import javax.measure.unit.Unit;
 
 public class ConstraintList<Q extends Quantity> extends AbstractList<Constraint<Q>> {
+    public static <Q extends Quantity> Builder<Q> create(Unit<Q> unit) {
+        return new Builder<Q>(unit);
+    }
+
+    public static class Builder<Q extends Quantity> {
+        private final Unit<Q> defaultUnit;
+        private final List<Constraint<Q>> powerConstraints;
+
+        public Builder(Unit<Q> defaultUnit) {
+            this.defaultUnit = defaultUnit;
+            powerConstraints = new ArrayList<Constraint<Q>>();
+        }
+
+        public Builder<Q> addSingle(Measurable<Q> value) {
+            powerConstraints.add(new Constraint<Q>(value));
+            return this;
+        }
+
+        public Builder<Q> addSingle(double value) {
+            powerConstraints.add(new Constraint<Q>(Measure.valueOf(value, defaultUnit)));
+            return this;
+        }
+
+        public Builder<Q> addRange(Measurable<Q> lowerBound, Measurable<Q> upperBound) {
+            powerConstraints.add(new Constraint<Q>(lowerBound, upperBound));
+            return this;
+        }
+
+        public Builder<Q> addRange(double lowerBound, double upperBound) {
+            powerConstraints.add(new Constraint<Q>(Measure.valueOf(lowerBound, defaultUnit),
+                                                   Measure.valueOf(upperBound, defaultUnit)));
+            return this;
+        }
+
+        public ConstraintList<Q> build() {
+            return new ConstraintList<Q>(powerConstraints);
+        }
+    }
+
     private final Constraint<Q>[] powerConstraints;
 
     @SuppressWarnings("unchecked")
     public ConstraintList(List<Constraint<Q>> list) {
         powerConstraints = list.toArray(new Constraint[list.size()]);
-    }
-
-    public ConstraintList(Constraint<Q>... list) {
-        powerConstraints = Arrays.copyOf(list, list.length);
     }
 
     @Override
