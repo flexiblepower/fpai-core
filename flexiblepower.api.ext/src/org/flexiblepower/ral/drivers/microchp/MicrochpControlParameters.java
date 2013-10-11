a7f699b90212bc2ef2d68f6c36291dff8d0230db
Marc de Jonge <marc.dejonge@tno.nl>
2013-10-11 14:58:06 +0200

    Deleted the driver.microchp, it is not ready yet

--- flexiblepower.api.ext/src/org/flexiblepower/ral/drivers/microchp/MicrochpControlParameters.java
+++
@@ -1,7 +0,0 @@
-package org.flexiblepower.ral.drivers.microchp;
-
-import org.flexiblepower.ral.ResourceControlParameters;
-
-public interface MicrochpControlParameters extends ResourceControlParameters {
-    boolean systemOn();
-}
--- flexiblepower.api.ext/src/org/flexiblepower/ral/drivers/microchp/MicrochpDriver.java
+++
@@ -1,7 +0,0 @@
-package org.flexiblepower.ral.drivers.microchp;
-
-import org.flexiblepower.ral.ResourceDriver;
-
-public interface MicrochpDriver extends ResourceDriver<MicrochpState, MicrochpControlParameters> {
-
-}
