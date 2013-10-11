8ed7bba11dc12f7417b55dc31ffe90ddbb6e9773
Marc de Jonge <marc.dejonge@tno.nl>
2013-10-11 15:00:55 +0200

    Documented the driver.refrigerator package

--- flexiblepower.api.ext/src/org/flexiblepower/ral/drivers/refrigerator/RefrigeratorState.java
+++ flexiblepower.api.ext/src/org/flexiblepower/ral/drivers/refrigerator/RefrigeratorState.java
@@ -2,12 +2,28 @@ package org.flexiblepower.ral.drivers.refrigerator;
 
 import org.flexiblepower.ral.ResourceState;
 
+/**
+ * The state of an refrigerator.
+ */
 public interface RefrigeratorState extends ResourceState {
+    /**
+     * @return The current temperature in the refrigerator in degrees Celcius.
+     */
     double getCurrentTemperature();
 
+    /**
+     * @return The target temperature in the refrigerator in degrees Celcius. The current temperature should always be
+     *         lower than the target.
+     */
     double getTargetTemperature();
 
+    /**
+     * @return The minimum temperature in the refrigerator in degrees Celcius.
+     */
     double getMinimumTemperature();
 
+    /**
+     * @return true when super cool is currently on, false if not.
+     */
     boolean getSupercoolMode();
 }
