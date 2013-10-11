2926029a6250b0b2a1bba4c3eab45112bc926c73
Marc de Jonge <marc.dejonge@tno.nl>
2013-10-11 14:51:20 +0200

    Documented the ral.wiring package

---
+++ flexiblepower.api.ext/src/org/flexiblepower/ral/wiring/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains interfaces for the {@link org.flexiblepower.ral.wiring.ResourceWiringManager}.
+ */
+@Version("1.0.0.${qualifier}")
+package org.flexiblepower.ral.wiring;
+
+import aQute.bnd.annotation.Version;
+
