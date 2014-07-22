fb6169f9cbb55ac5cdc0ebf91ce2c9e027b5ba4a
Marc de Jonge <marc.dejonge@tno.nl>
2014-07-22 11:32:30 +0200

    Added the startTime to the dishwasher state
    Also extended the JavaDoc to describe what null values in the state mean

--- flexiblepower.api.ext/src/org/flexiblepower/ral/drivers/dishwasher/DishwasherState.java
+++ flexiblepower.api.ext/src/org/flexiblepower/ral/drivers/dishwasher/DishwasherState.java
@@ -9,17 +9,34 @@ import org.flexiblepower.efi.util.CommodityProfile;
 import org.flexiblepower.ral.ResourceState;
 
 /**
- * The {@link DishwasherState} that describes.
+ * The {@link DishwasherState} that describes the current state of a dishwasher. In general there are 3 options:
+ *
+ * <ul>
+ * <li><code>latestStartTime == null && startTime == null && program == null</code><br/>
+ * This means that the dishwasher is off or no program has been selected. In this case there is nothing to control.</li>
+ * <li><code>latestStartTime != null && startTime == null && program != null</code><br/>
+ * This means that the dishwasher is on with a selected program that has been delayed. It can be started anywhere
+ * between now and the latestStartTime.</li>
+ * <li><code>startTime != null && program != null</code><br />
+ * This means that the program has started at the startTime and it can not be interrupted anymore.
+ * </ul>
  */
 public interface DishwasherState extends ResourceState {
     /**
-     * @return The latest time at which the current program should start.
+     * @return The latest time at which the current program should start, or <code>null</code> if there is nothing to
+     *         start.
      */
     Date getLatestStartTime();
 
     /**
+     * @return The time at which the program has been started, or <code>null</code> if it hasn't been started yet.
+     */
+    Date getStartTime();
+
+    /**
      * @return The unique name of the selected program. This will be used for display purposes and in the
-     *         {@link DishwasherControlParameters#getProgram()} to make sure that this program will be started.
+     *         {@link DishwasherControlParameters#getProgram()} to make sure that this program will be started. This can
+     *         be <code>null</code> when the dishwasher is off or no program has been selected.
      */
     String getProgram();
 
