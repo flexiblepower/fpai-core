2afe2a71b0866aba60e150d9006ceba4637a092b
Mente Konsman <mente.konsman@tno.nl>
2014-07-25 14:06:53 +0200

    Alternative solution for including curtailment information in the UncontrolledRegistration class.

--- flexiblepower.api.efi/src/org/flexiblepower/efi/uncontrolled/UncontrolledRegistration.java
+++ flexiblepower.api.efi/src/org/flexiblepower/efi/uncontrolled/UncontrolledRegistration.java
@@ -1,28 +1,25 @@
 package org.flexiblepower.efi.uncontrolled;
 
 import java.util.Date;
-import java.util.Set;
+import java.util.Map;
 
 import org.flexiblepower.rai.comm.ControlSpaceRegistration;
 import org.flexiblepower.rai.values.Commodity;
+import org.flexiblepower.rai.values.ConstraintList;
 
 public class UncontrolledRegistration extends ControlSpaceRegistration {
 
-    private final Set<Commodity> supportedCommodities;
-
-    private final Set<CurtailmentInfo> curtailmentInfoSet;
+    private final Map<Commodity, ConstraintList> supportedCommodityCurtailments;
 
     public UncontrolledRegistration(String resourceId,
                                     Date timestamp,
-                                    Set<Commodity> supportedCommodities,
-                                    Set<CurtailmentInfo> curtailmentInfoSet) {
+                                    Map<Commodity, ConstraintList> supportedCommodityCurtailments) {
         super(resourceId, timestamp);
-        this.supportedCommodities = supportedCommodities;
-        this.curtailmentInfoSet = curtailmentInfoSet;
+        this.supportedCommodityCurtailments = supportedCommodityCurtailments;
     }
 
-    public Set<Commodity> getSupportedCommodities() {
-        return supportedCommodities;
+    public Map<Commodity, ConstraintList> getSupportedCommodities() {
+        return supportedCommodityCurtailments;
     }
 
 }
