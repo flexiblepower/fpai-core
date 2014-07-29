b33a6b650dc321a85a83a4a7d7c7a1677a072530
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2014-07-29 15:37:49 +0200

    Removed id and name from leakagefunction

--- flexiblepower.api.efi/src/org/flexiblepower/efi/buffer/LeakageFunction.java
+++ flexiblepower.api.efi/src/org/flexiblepower/efi/buffer/LeakageFunction.java
@@ -2,22 +2,8 @@ package org.flexiblepower.efi.buffer;
 
 public class LeakageFunction extends FillLevelFunction<RangeElement> {
 
-    private final int id;
-
-    private final String name;
-
-    public LeakageFunction(int id, String name, RangeElement[] rangeElements) {
+    public LeakageFunction(RangeElement[] rangeElements) {
         super(rangeElements);
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
 }
