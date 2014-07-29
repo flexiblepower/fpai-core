c399f49eb8b5133c9eda618eb01d620e32ad992a
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2014-07-29 12:59:21 +0200

    Fixed uncontrolledMeasurement

--- flexiblepower.api.efi/src/org/flexiblepower/efi/uncontrolled/UncontrolledMeasurement.java
+++ flexiblepower.api.efi/src/org/flexiblepower/efi/uncontrolled/UncontrolledMeasurement.java
@@ -1,17 +1,23 @@
 package org.flexiblepower.efi.uncontrolled;
 
+import java.util.Date;
 import java.util.Map;
 
 import javax.measure.Measurable;
+import javax.measure.quantity.Duration;
 
 import org.flexiblepower.rai.values.Commodity;
 
-public class UncontrolledMeasurement {
+public class UncontrolledMeasurement extends UncontrolledUpdate {
 
     private final Map<Commodity, Measurable> measurements;
 
-    public UncontrolledMeasurement(Map<Commodity, Measurable> measurements) {
-        super();
+    public UncontrolledMeasurement(String resourceId,
+                                   Date timestamp,
+                                   Date validFrom,
+                                   Measurable<Duration> allocationDelay,
+                                   Map<Commodity, Measurable> measurements) {
+        super(resourceId, timestamp, validFrom, allocationDelay);
         this.measurements = measurements;
     }
 
