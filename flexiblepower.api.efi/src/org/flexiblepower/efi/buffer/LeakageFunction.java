501ea0c22f19a5faf58983e08e31d45d3908ccc8
Marc de Jonge <marc.dejonge@tno.nl>
2014-08-26 10:55:25 +0200

    Added builder for the LeakageFunction

--- flexiblepower.api.efi/src/org/flexiblepower/efi/buffer/LeakageFunction.java
+++ flexiblepower.api.efi/src/org/flexiblepower/efi/buffer/LeakageFunction.java
@@ -1,8 +1,35 @@
 package org.flexiblepower.efi.buffer;
 
-public class LeakageFunction extends FillLevelFunction<RangeElement> {
+import java.util.ArrayList;
+import java.util.List;
 
-    public LeakageFunction(RangeElement[] rangeElements) {
+public class LeakageFunction extends FillLevelFunction<RangeElement> {
+    public static Builder create() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final List<RangeElement> elements = new ArrayList<RangeElement>();
+
+        Builder() {
+        }
+
+        public Builder add(RangeElement element) {
+            elements.add(element);
+            return this;
+        }
+
+        public Builder add(double lowerBound, double upperBound, double fillingSpeed) {
+            elements.add(new RangeElement(lowerBound, upperBound, fillingSpeed));
+            return this;
+        }
+
+        public LeakageFunction build() {
+            return new LeakageFunction(elements.toArray(new RangeElement[elements.size()]));
+        }
+    }
+
+    public LeakageFunction(RangeElement... rangeElements) {
         super(rangeElements);
     }
 
