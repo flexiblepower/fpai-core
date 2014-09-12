0919c3d85daab25f948da9fb3220344b4617192e
Marc de Jonge <marc.dejonge@tno.nl>
2014-09-12 14:08:38 +0200

    Fixed the unconstrained stuff

--- flexiblepower.api.efi/src/org/flexiblepower/efi/unconstrained/UnconstrainedRegistration.java
+++ flexiblepower.api.efi/src/org/flexiblepower/efi/unconstrained/UnconstrainedRegistration.java
@@ -8,8 +8,10 @@ import javax.measure.quantity.Duration;
 import org.flexiblepower.rai.ControlSpaceRegistration;
 import org.flexiblepower.rai.values.CommoditySet;
 
+/**
+ * This class is derived from ControlSpaceRegistration and contains the registration items that are unique to a buffer.
+ */
 public class UnconstrainedRegistration extends ControlSpaceRegistration {
-
     private final CommoditySet supportedCommodities;
 
     public UnconstrainedRegistration(String resourceId,
@@ -17,11 +19,39 @@ public class UnconstrainedRegistration extends ControlSpaceRegistration {
                                      Measurable<Duration> allocationDelay,
                                      CommoditySet supportedCommodities) {
         super(resourceId, timestamp, allocationDelay);
+        if (supportedCommodities == null) {
+            throw new NullPointerException("supportedCommodities");
+        } else if (supportedCommodities.isEmpty()) {
+            throw new IllegalArgumentException("supportedCommodities is empty");
+        }
         this.supportedCommodities = supportedCommodities;
     }
 
+    /**
+     * @return The set of all commodities that can be produced or consumed by this appliance
+     */
     public CommoditySet getSupportedCommodities() {
         return supportedCommodities;
     }
 
+    @Override
+    public int hashCode() {
+        return 31 * super.hashCode() + supportedCommodities.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!super.equals(obj)) {
+            return false;
+        }
+
+        UnconstrainedRegistration other = (UnconstrainedRegistration) obj;
+        return other.supportedCommodities.equals(supportedCommodities);
+    }
+
+    @Override
+    protected void toString(StringBuilder sb) {
+        super.toString(sb);
+        sb.append("supportedCommodities=").append(supportedCommodities).append(", ");
+    }
 }
--- flexiblepower.api.efi/src/org/flexiblepower/efi/unconstrained/UnconstrainedUpdate.java
+++ flexiblepower.api.efi/src/org/flexiblepower/efi/unconstrained/UnconstrainedUpdate.java
@@ -2,17 +2,25 @@ package org.flexiblepower.efi.unconstrained;
 
 import java.util.Date;
 
-import javax.measure.Measurable;
-import javax.measure.quantity.Duration;
-
 import org.flexiblepower.rai.ControlSpaceUpdate;
+import org.flexiblepower.time.TimeService;
 
+/**
+ * This class is derived from {@link ControlSpaceUpdate} and is used to bundle all updates from an appliance of the
+ * unconstrained category.
+ */
 public class UnconstrainedUpdate extends ControlSpaceUpdate {
-
-    private static final long serialVersionUID = 3798980923200359354L;
-
-    public UnconstrainedUpdate(String resourceId, Date timestamp, Date validFrom, Measurable<Duration> allocationDelay) {
-        super(resourceId, timestamp, validFrom, allocationDelay);
+    /**
+     * Constructs a new {@link UnconstrainedUpdate} message with the specific validFrom
+     *
+     * @param resourceId
+     *            The resource identifier
+     * @param timestamp
+     *            The moment when this constructor is called (should be {@link TimeService#getTime()}
+     * @param validFrom
+     *            This timestamp indicates from which moment on this update is valid.
+     */
+    public UnconstrainedUpdate(String resourceId, Date timestamp, Date validFrom) {
+        super(resourceId, timestamp, validFrom);
     }
-
 }
