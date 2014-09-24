6557ae434a078d3eb380d3ed4daac324d7be61c5
Jan Kazemier <jan.kazemier@tno.nl>
2014-09-24 16:11:57 +0200

    Fixed menu's and added info on selected items (edges, nodes) in connection manager

--- flexiblepower.felix.webconsole.plugins/res/connectionManager.js
+++ flexiblepower.felix.webconsole.plugins/res/connectionManager.js
@@ -1,84 +1,162 @@
 cy = cytoscape({
-  container: document.getElementById('cy'),
-  selectionType: 'single', // (isTouchDevice ? 'additive' : 'single'),
-//  layout: {
-//	    name: 'circle',
-//	    padding: 10
-//	  },
-  layout: { name: 'breadthfirst', fit: true },
-  zoom: 1,
-  minZoom: 0,
-  maxZoom: 100,
-  zoomingEnabled: true,
-  userZoomingEnabled: true,
-  pan: { x: 0, y: 0 },
-  panningEnabled: true,
-  userPanningEnabled: true,
-  autoungrabifyNodes: false,
-  hideEdgesOnViewport: false,
-  hideLabelsOnViewport: false,
-  textureOnViewport: false,
-  
-  style: [
-          {
-            selector: 'node',
-            css: {
-                'content': 'data(name)',
-                'font-family': 'helvetica',
-                'font-size': 14,
-                'text-outline-width': 2,
-                'text-outline-color': '#000',
-                'text-valign': 'center',
-                'color': '#fff',
-                'background-color': '#0680C1',
-                'border-color': '#000'
-              }
-          },
-          {
-            selector: ':selected',
-            css: {
-              'background-color': '#693',
-              'line-color': '#693',
-              'text-outline-width': 3,
-              'text-outline-color': '#693'
-            }
-          },
-          {
-            selector: ':parent',
-            css: {
-              'background-color': '#041B50',
-              'line-color': '#000',
-              'text-outline-color': '#000'
-            }
-          },
-          {
-            selector: 'edge',
-            css: {
-              'width': 5,
-              'target-arrow-shape': 'none'
-              
-            }
-          }
-        ],
- 
-  ready: function(){
-  }
-} );
+	container : document.getElementById('cy'),
+	selectionType : 'single', // (isTouchDevice ? 'additive' : 'single'),
+	// layout: {
+	// name: 'circle',
+	// padding: 10
+	// },
+	layout : {
+		name : 'cose',
+		padding : 100
+	},
+	zoom : 1,
+	minZoom : 0,
+	maxZoom : 100,
+	zoomingEnabled : true,
+	userZoomingEnabled : true,
+	pan : {
+		x : 0,
+		y : 0
+	},
+	panningEnabled : true,
+	userPanningEnabled : true,
+	autoungrabifyNodes : false,
+	hideEdgesOnViewport : false,
+	hideLabelsOnViewport : false,
+	textureOnViewport : false,
 
-$.get("/system/console/fpai-connection-manager/getGraph.json", function(json){
-	console.log(json);
-	cy.load(json);
-	console.log("connected:");
-	console.log(cy.edges("[isconnected]"));
-}).fail(function(jqXHR, textStatus, errorThrown){
-	console.log("error: " + textStatus + ": " + errorThrown);
+	style : [ {
+		selector : 'node',
+		css : {
+			'content' : 'data(name)',
+			'font-family' : 'helvetica',
+			'font-size' : 14,
+			'text-outline-width' : 2,
+			'text-outline-color' : '#000',
+			'text-valign' : 'center',
+			'color' : '#fff',
+			'background-color' : '#0680C1',
+			'border-color' : '#000'
+		}
+	}, {
+		selector : ':selected',
+		css : {
+			'background-color' : '#693',
+			'line-color' : '#693',
+			'text-outline-width' : 3,
+			'text-outline-color' : '#693'
+		}
+	}, {
+		selector : ':parent',
+		css : {
+			'background-color' : '#041B50',
+			'line-color' : '#000',
+			'text-outline-color' : '#000'
+		}
+	}, {
+		selector : 'edge',
+		css : {
+			'width' : 5,
+			'target-arrow-shape' : 'none'
+
+		}
+	}, {
+		selector : '.faded',
+		css : {
+			'opacity' : 0.95,
+			'text-opacity' : 0.95
+		}
+	}, {
+		selector : '.isconnected',
+		css : {
+			'width' : 6,
+			'line-color' : '#0F0',
+			'target-arrow-shape' : 'none'
+
+		}
+	} ],
+
+	ready : function() {
+		$("#cy div").css("z-index","-1"); // fixes the menu
+	}
+});
+function refresh() {
+	$.get("/system/console/fpai-connection-manager/getGraph.json",
+			function(json) {
+				console.log(json);
+				cy.load(json);
+				console.log("connected:");
+				console.log(cy.edges("[isconnected]"));
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+		console.log("error: " + textStatus + ": " + errorThrown);
+	});
+}
+refresh();
+
+function autoconnect() {
+	console.log("Autoconnecting..");
+	$.post("/system/console/fpai-connection-manager/autoconnect.json",
+			function(result) {
+				console.log(result);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+		console.log("error: " + textStatus + ": " + errorThrown);
+	});
+	refresh();
+}
+
+cy.on('tap', 'node', function(e) {
+	var node = e.cyTarget;
+	var neighborhood = node.neighborhood().add(node);
+	cy.elements().addClass('faded');
+	neighborhood.removeClass('faded');
+
+	onNodeSelected(node.id());
 });
 
-function autoconnect(){
-   console.log("Autoconnecting..");
-   $.post("/system/console/fpai-connection-manager/autoconnect.json", function(result){
-	   console.log(result);
-   }).fail(function(jqXHR, textStatus, errorThrown){
-		console.log("error: " + textStatus + ": " + errorThrown);
-   });
+cy.on('tap', 'edge', function(e) {
+	var edge = e.cyTarget;
+	var neighborhood = edge.connectedNodes().add(edge);
+	cy.elements().addClass('faded');
+	neighborhood.removeClass('faded');
+
+	onEdgeSelected(edge.id());
+});
+
+cy.on('tap', function(e) {
+	if (e.cyTarget === cy) {
+		cy.elements().removeClass('faded');
+		onNothingSelected();
+	}
+});
+
+function onNothingSelected() {
+	$(".infotable").text("");
+	$("#1a").text("Nothing selected");
 }
+
+function onEdgeSelected(elemid) {
+	var elem = cy.$('#' + elemid); // select element
+	$(".infotable").text("");
+	$("#1a").text("Possible connection");
+	$("#2a").text("connects: ");
+	$("#2b").text(elem.data("source"));
+	$("#3a").text("with: ");
+	$("#3b").text(elem.data("target"));
+	$("#4a").text("Connected: ");
+	$("#4b").text(elem.data("isconnected"));
+	
+	console.log(elem.data());
+}
+
+function onNodeSelected(elemid) {
+	var elem = cy.$('#' + elemid); // select element
+	$(".infotable").text("");
+	$("#1a").text("node");
+	console.log(elem.data());
+}
+
+function updateInfo(elemid) {
+	
+	
+}
+
