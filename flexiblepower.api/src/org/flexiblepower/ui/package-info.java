16f26d5fb6aece0384bad85bbf23aa18b61dfa7a
Marc de Jonge <marc.dejonge@tno.nl>
2014-12-11 18:35:35 +0100

    Updated the package version of org.flexiblepower.ui

--- flexiblepower.api/src/org/flexiblepower/ui/package-info.java
+++ flexiblepower.api/src/org/flexiblepower/ui/package-info.java
@@ -1,7 +1,7 @@
 /**
  * The UI package provides the {@link org.flexiblepower.ui.Widget} interface.
  */
-@Version("1.0.0.${qualifier}")
+@Version("1.1.0.${qualifier}")
 package org.flexiblepower.ui;
 
 import aQute.bnd.annotation.Version;
