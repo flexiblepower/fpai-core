ba3d623f4a1ab4365e079c8bef6f33db89a63092
Marc de Jonge <marc.dejonge@tno.nl>
2014-12-12 20:20:57 +0100

    Move to shared gradle config

--- cnf/deployment/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Running from `pwd`
-
-java \
-	-Djava.security.policy=etc/all.policy \
-	-Dfelix.config.properties=file:etc/config.properties \
-	-Dfelix.cm.dir="`pwd`/config" \
-	-Dlogback.configurationFile=etc/logback.xml \
-	-jar org.apache.felix.main.jar
