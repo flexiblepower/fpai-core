5c18151480621ac946bc6c158bb774e51d50c1f2
Marc de Jonge <marc.dejonge@tno.nl>
2014-12-23 14:27:36 +0100

    Made the test more robust, no longer dependant on waiting

--- flexiblepower.runtime/test/org/flexiblepower/simulation/test/SchedulerTest.java
+++ flexiblepower.runtime/test/org/flexiblepower/simulation/test/SchedulerTest.java
@@ -4,13 +4,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.flexiblepower.simulation.Simulation;
 import org.flexiblepower.simulation.scheduling.SimulatedScheduleService;
 
 public class SchedulerTest extends TestCase {
-    public void testRecurringTask() {
+    public void testRecurringTask() throws InterruptedException {
         SimulatedScheduleService scheduler = new SimulatedScheduleService();
         scheduler.activate();
 
@@ -22,17 +22,20 @@ public class SchedulerTest extends TestCase {
             }
         }, 0, 1, TimeUnit.SECONDS);
 
-        Date d1_1_2012 = new Date(1325376000000l);
+        Date startDate = new Date(1325376000000L); // 01-01-2012 00:00:00.000
+        Date endDate = new Date(1325376059999L); // 01-01-2012 00:00:59.999
 
         runCounter.set(0);
 
-        scheduler.startSimulation(d1_1_2012, 2);
-        try {
-            Thread.sleep(2250);
-        } catch (InterruptedException e) {
+        scheduler.startSimulation(startDate, endDate, 60);
+        long startTime = System.currentTimeMillis();
+        while (scheduler.getSimulationClockState() != Simulation.State.STOPPED) {
+            Thread.sleep(10);
         }
-        scheduler.stopSimulation();
+        long duration = System.currentTimeMillis() - startTime;
 
-        Assert.assertEquals(5, runCounter.get());
+        assertEquals(60, runCounter.get());
+        System.out.printf("Running took %dms, expected around 1 sec%n", duration);
+        assertTrue(duration > 950 && duration < 1150);
     }
 }
