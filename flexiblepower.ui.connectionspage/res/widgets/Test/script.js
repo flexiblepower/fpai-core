846add88c3d8c55918c2cd37bdf06a279c0f4c38
Marc de Jonge <marc.dejonge@tno.nl>
2015-04-24 11:55:28 +0200

    Added easier function to dynamically create endpoints with ports

--- flexiblepower.ui.connectionspage/res/widgets/Test/script.js
+++ flexiblepower.ui.connectionspage/res/widgets/Test/script.js
@@ -1,85 +1,118 @@
 jsPlumb.ready(function() {
-	jsPlumb.setContainer(document.querySelectorAll("div#main"))
-	
-	jsPlumb.registerEndpointTypes({
-		"disabled": { paintStyle: {
-			strokeStyle: "red"
-		}}
+	var instance = jsPlumb.getInstance({
+//		ConnectionOverlays: [
+//            [ "Label", { label: "FOO", id: "label", cssClass: "connectionLabel", enabled: false } ]
+//		],
+		Container: "main"
 	});
 	
-	var endpointProps = {
-        endpoint: "Dot",
-        paintStyle: {
-            strokeStyle: "rgba(122, 176, 44, 0.8)",
-            fillStyle: "transparent",
-            radius: 10,
-            lineWidth: 3
-        },
-        isSource: true,
-        isTarget: true,
-        connector: [ "Flowchart", { 
-        	stub: 20, 
-        	gap: 10, 
-        	cornerRadius: 5, 
-        	alwaysRespectStubs: false } ],
-        connectorHoverStyle: {
-            lineWidth: 4,
-            strokeStyle: "#216477",
-            outlineWidth: 2,
-            outlineColor: "white"
-        },
-        dragOptions: {},
-        anchor:["Continuous", { faces:[ "bottom", "top" ] } ]
-	};
+	instance.registerEndpointType("disabled", { paintStyle: { strokeStyle: "red" } });
 	
-	jsPlumb.batch(function() {
-		var ep = null;
+	var _createTextNode = function(text, isSmall) {
+		var node = document.createElement("p");
+		if(isSmall) {
+			node.className = "small";
+		}
+		node.innerHTML = text;
+		return node;
+	}
+	
+	var _addEndpoint = function(endpointId, ports) {
+		// First check if a node with that ID already exists, then just return
+		if(document.getElementById(endpointId) != null) {
+			return;
+		}
 		
-		ep = jsPlumb.addEndpoint("powerMatcher", endpointProps, {
-			uuid: "powerMatcher:timeshifter",
-			label: "timeshifter"
+		// Parse the id to split it up into a package, a name and an id
+		var pkgName = "", epName = endpointId, epId = "";
+		
+		var data = endpointId.match(/^((?:[a-z]+\.)*)([a-zA-Z]+)(?:\.([^\.]+))$/);
+		if(data != null) {
+			pkgName = data[1];
+			epName = data[2];
+			epId = data[3];
+		} else {
+			data = endpointId.match(/^((?:[^\.]+\.)*)([^\.]+)$/);
+			if(data != null) {
+				pkgName = data[1];
+				epName = data[2];
+				epId = "";
+			}
+		}
+		
+		// Now create the node
+		var node = document.createElement("div");
+		node.className = "node";
+		node.id = endpointId;
+		node.appendChild(_createTextNode(pkgName, true));
+		node.appendChild(_createTextNode(epName, false));
+		node.appendChild(_createTextNode(epId, true));
+		node.style.left = (Math.random() * 1000) + "px";
+		node.style.top = (Math.random() * 500) + "px";
+		
+		// Add the node to the document
+		document.getElementById("main").appendChild(node);
+		
+		// Now create the ports
+		for(portId in ports) {
+			var ep = instance.addEndpoint(endpointId, {
+		        endpoint: "Dot",
+		        isSource: true,
+		        isTarget: true,
+		        connector: [ "Flowchart", { stub: 20, gap: 10, cornerRadius: 5} ],
+		        paintStyle: {
+		            strokeStyle: "#216477",
+		            fillStyle: "transparent",
+		            radius: 10,
+		            lineWidth: 3
+		        },
+		        hoverPaintStyle: {
+		            fillStyle: "#216477"
+		        },
+		        anchor: ["Continuous", { faces:[ "bottom", "top" ] } ],
+		        uuid: endpointId + ":" + portId,
+		        label: portId,
+		        parameters: { potentialTargets: ports[portId] },
+			});
+		}
+	}
+	
+	instance.batch(function() {
+		_addEndpoint("net.powermatcher.fpai.controller.PowerMatcherController.auctioneer", {
+			timeshifter: ["org.flexblepower.manager.dishwasher.DishwasherManager.1:controller"],
+			buffer: [],
+			unconstrained: [],
+			uncontrolled: []
 		});
-		ep.setParameter("potentialTargets", ["dishwasher-manager:controller"]);
-		
-		ep = jsPlumb.addEndpoint("dishwasher-manager", endpointProps, {
-			uuid: "dishwasher-manager:driver",
-			label: "driver"
+		_addEndpoint("org.flexblepower.manager.dishwasher.DishwasherManager.1", {
+			controller: ["net.powermatcher.fpai.controller.PowerMatcherController.auctioneer:timeshifter"],
+			driver: ["org.flexiblepower.driver.dishwasher.DishwasherDriver.1:manager"]
 		});
-		ep.setParameter("potentialTargets", ["dishwasher-driver:manager"]);
-		
-		ep = jsPlumb.addEndpoint("dishwasher-manager", endpointProps, {
-			uuid: "dishwasher-manager:controller",
-			label: "controller"
+		_addEndpoint("org.flexiblepower.driver.dishwasher.DishwasherDriver.1", {
+			manager: ["org.flexblepower.manager.dishwasher.DishwasherManager.1:driver"]
 		});
-		ep.setParameter("potentialTargets", ["powerMatcher:timeshifter"]);
 		
-		ep = jsPlumb.addEndpoint("dishwasher-driver", endpointProps, {
-			uuid: "dishwasher-driver:manager",
-			label: "manager"			
-		});
-		ep.setParameter("potentialTargets", ["dishwasher-manager:driver"]);
+		instance.draggable(document.querySelectorAll(".node"), { grid: [200, 100] });
 		
-		jsPlumb.draggable(document.querySelectorAll(".node"), { grid: [20, 20] });
-		
-		jsPlumb.bind("beforeDrag", function(params) {
-			jsPlumb.batch(function() {
-				jsPlumb.selectEndpoints().setEnabled(false);
-				jsPlumb.selectEndpoints().setType("disabled");
+		instance.bind("beforeDrag", function(params) {
+			instance.batch(function() {
+				instance.selectEndpoints().setEnabled(false);
+				instance.selectEndpoints().setType("disabled");
 				
 				params.endpoint.clearTypes();
 				
 				var targets = params.endpoint.getParameter("potentialTargets");
 				for(ix in targets) {
-					var target = jsPlumb.getEndpoint(targets[ix]);
+					var target = instance.getEndpoint(targets[ix]);
 					target.setEnabled(true);
 					target.clearTypes();
 				}
 			});
 		});
 		
-		jsPlumb.bind("connectionDragStop", function(params) {
-			jsPlumb.selectEndpoints().setEnabled(true);
-			jsPlumb.selectEndpoints().each(function(ep) {ep.clearTypes();});
+		instance.bind("connectionDragStop", function(params) {
+			instance.selectEndpoints().setEnabled(true);
+			instance.selectEndpoints().each(function(ep) {ep.clearTypes();});
 		});
 	});
 	
