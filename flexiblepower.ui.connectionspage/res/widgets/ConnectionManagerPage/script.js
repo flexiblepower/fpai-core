5421515fc885a99f6b57ddea795a753dde36993d
Marc de Jonge <marc.dejonge@tno.nl>
2015-04-24 16:26:27 +0200

    Now added properties and automatic layout

--- flexiblepower.ui.connectionspage/res/widgets/ConnectionManagerPage/script.js
+++ flexiblepower.ui.connectionspage/res/widgets/ConnectionManagerPage/script.js
@@ -17,22 +17,22 @@ jsPlumb.ready(function() {
 		return node;
 	}
 	
-	var _addEndpoint = function(endpointId, ports, style) {
+	var _addEndpoint = function(endpoint) {
 		// First check if a node with that ID already exists, then just return
-		if(document.getElementById(endpointId) != null) {
+		if(document.getElementById(endpoint.id) != null) {
 			return;
 		}
 		
 		// Parse the id to split it up into a package, a name and an id
-		var pkgName = "", epName = endpointId, epId = "";
+		var pkgName = "", epName = endpoint.id, epId = "";
 		
-		var data = endpointId.match(/^((?:[a-z]+\.)*)([a-zA-Z]+)(?:\.([^\.]+))$/);
+		var data = endpoint.id.match(/^((?:[a-z]+\.)*)([a-zA-Z]+)(?:\.([^\.]+))$/);
 		if(data != null) {
 			pkgName = data[1];
 			epName = data[2];
 			epId = data[3];
 		} else {
-			data = endpointId.match(/^((?:[^\.]+\.)*)([^\.]+)$/);
+			data = endpoint.id.match(/^((?:[^\.]+\.)*)([^\.]+)$/);
 			if(data != null) {
 				pkgName = data[1];
 				epName = data[2];
@@ -41,96 +41,86 @@ jsPlumb.ready(function() {
 		}
 		
 		// Now create the node
-		var node = document.createElement("div");
-		node.className = "node";
-		node.id = endpointId;
-		node.appendChild(_createTextNode(pkgName, true));
-		node.appendChild(_createTextNode(epName, false));
-		node.appendChild(_createTextNode(epId, true));
+		var node = $('<div class="node" id="' + endpoint.id + '"><p class="small">' + pkgName + '</p><p>' + epName + '</p><p class="small">' + epId + '</p></div>');
 		
-		for(key in style) {
-			node.style[key] = style[key];
+//		node.hover(function() {
+//			for(ix in endpoint.properties) {
+//				var prop = endpoint.properties[ix];
+//				node.appendChild(_createTextNode(prop, true));
+//			}
+//		});
+		if(endpoint.properties) {
+			for(ix in endpoint.properties) {
+				prop = endpoint.properties[ix];
+				$('<p class="small">' + prop + "</p>").appendTo(node);
+			}
+		}
+		
+		for(key in endpoint.style) {
+			node.css(key, endpoint.style[key]);
 		}
 		
 		// Add the node to the document
-		document.getElementById("main").appendChild(node);
+		node.appendTo($("#main"));
 		
 		// Make it draggable
 		instance.draggable(node);
 				
 		// Now create the ports
-		for(portId in ports) {			
-			var ep = instance.addEndpoint(endpointId, {
+		for(ix in endpoint.ports) {
+			port = endpoint.ports[ix];
+			var ep = instance.addEndpoint(endpoint.id, {
 		        endpoint: "Dot",
 		        isSource: true,
 		        isTarget: true,
 		        connector: [ "Bezier", { curviness: 50, gap: 10 }], // [ "Flowchart", { stub: 20, gap: 10, cornerRadius: 5} ],
 		        paintStyle: {
-		            strokeStyle: "#216477",
-		            fillStyle: "#216477",
-		            radius: 10,
-		            lineWidth: 3,
-		        },
-		        hoverPaintStyle: {
-		            fillStyle: "#216477",
+		            strokeStyle: "rgb(122,193,3)",
+		            fillStyle: "rgb(122,193,3)",
+		            radius: 15,
 		            lineWidth: 3,
 		        },
 		        connectorStyle: {
-		        	strokeStyle: "#216477",
+		        	strokeStyle: "rgb(122,193,3)",
+		        	lineWidth: 3,
 		        },
 		        anchor: ["Continuous", { faces:[ "bottom", "top" ] } ],
-		        uuid: endpointId + ":" + portId,
-		        label: portId,
-		        parameters: { potentialTargets: ports[portId] },
+		        uuid: endpoint.id + ":" + port.id,
+		        label: port.id,
+		        parameters: { potentialTargets: port.potentialConnections },
 			});
 		}
 	}
 	
-	instance.batch(function() {
-		_addEndpoint("net.powermatcher.fpai.controller.PowerMatcherController.auctioneer", {
-			timeshifter: ["org.flexblepower.manager.dishwasher.DishwasherManager.1:controller"],
-			buffer: [],
-			unconstrained: [],
-			uncontrolled: []
-		}, {
-			left: "50px",
-			top: "30px",
-			width: "320px",
-		});
-		_addEndpoint("org.flexblepower.manager.dishwasher.DishwasherManager.1", {
-			controller: ["net.powermatcher.fpai.controller.PowerMatcherController.auctioneer:timeshifter"],
-			driver: ["org.flexiblepower.driver.dishwasher.DishwasherDriver.1:manager"]
-		}, {
-			left: "50px",
-			top: "230px",
-		});
-		_addEndpoint("org.flexiblepower.driver.dishwasher.DishwasherDriver.1", {
-			manager: ["org.flexblepower.manager.dishwasher.DishwasherManager.1:driver"]
-		}, {
-			left: "50px",
-			top: "430px",
-		});
-		
-		instance.bind("beforeDrag", function(params) {
+	$.ajax("currentState", {
+		success: function(data) {
 			instance.batch(function() {
-				instance.selectEndpoints().setEnabled(false);
-				instance.selectEndpoints().setType("disabled");
-				
-				params.endpoint.clearTypes();
-				
-				var targets = params.endpoint.getParameter("potentialTargets");
-				for(ix in targets) {
-					var target = instance.getEndpoint(targets[ix]);
-					target.setEnabled(true);
-					target.clearTypes();
+				for(ix in data.endpoints) {
+					_addEndpoint(data.endpoints[ix]);
 				}
+				
+				instance.bind("beforeDrag", function(params) {
+					instance.batch(function() {
+						instance.selectEndpoints().setEnabled(false);
+						instance.selectEndpoints().setType("disabled");
+						
+						params.endpoint.clearTypes();
+						
+						var targets = params.endpoint.potentialTargets;
+						for(ix in targets) {
+							var target = instance.getEndpoint(targets[ix]);
+							target.setEnabled(true);
+							target.clearTypes();
+						}
+					});
+				});
+				
+				instance.bind("connectionDragStop", function(params) {
+					instance.selectEndpoints().setEnabled(true);
+					instance.selectEndpoints().each(function(ep) {ep.clearTypes();});
+				});
 			});
-		});
-		
-		instance.bind("connectionDragStop", function(params) {
-			instance.selectEndpoints().setEnabled(true);
-			instance.selectEndpoints().each(function(ep) {ep.clearTypes();});
-		});
+		}
 	});
 	
 });
